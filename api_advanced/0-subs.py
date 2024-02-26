dad4d43ae34d9ec12f127c4965b424929ec38884
Pntawukamenya <p.ntawukame@alustudent.com>
2024-02-26 06:26:01 -0800

    Initial commit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-""" module for function to return number of subreddit subscribers """
 
 import requests
 
 def number_of_subscribers(subreddit):
+    """
+    Query the Reddit API and return the number of subscribers for a given subreddit.
+    If the subreddit is invalid, return 0.
+
+    Parameters:
+    subreddit (str): The name of the subreddit to query.
+
+    Returns:
+    int: The number of subscribers for the given subreddit, or 0 if the subreddit is invalid.
+    """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {
         'User-Agent': 'My User Agent 1.0',
