9757e5c005d0005dd94abb385520447e13090581
Palvis23 <139566126+Pntawukamenya@users.noreply.github.com>
2024-03-02 19:06:39 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,27 +1,13 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 
-""" Returns the number of subscribers for a given subreddit. """
+"""This module pulls information about a subreddit's subscribers"""
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """ Returns the number of subscribers for a given subreddit. """
-
-headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)\
-          AppleWebKit/537.36(KHTML, like Gecko) \
-         Chrome/90.0.4430.93 Safari/537.36'
-    }
-    params = {
-        'limit': 1,
-    }
-    url = f'https://www.reddit.com/r/{subreddit}/about/subscribers'
-    response = requests.get(url, headers=headers, params=params)
-
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
+    response = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit))
+    if response.statuscode == 200:
+        return response["subscribers"]
     else:
         return 0
+    
