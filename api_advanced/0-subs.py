86e94a4f7ec1e93f46088f216431ffbfcdd7e38e
Palvis23 <139566126+Pntawukamenya@users.noreply.github.com>
2024-03-02 19:10:02 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-"""This module pulls information about a subreddit's subscribers"""
+"""This module pulls information about a subreddit's subscribers."""
 
 import requests
 
 def number_of_subscribers(subreddit):
+"""Return number of subscribers."""
+
     response = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit))
     if response.statuscode == 200:
         return response["subscribers"]
     else:
         return 0
-    
