42a050e036fd7c713f1f1eb241fd47c42de18cec
Pntawukamenya <p.ntawukame@alustudent.com>
2024-03-03 12:40:47 +0000

    commit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,15 +1,32 @@
 #!/usr/bin/python3
 
-"""This module pulls information about a subreddit's subscribers."""
-
-import requests
+import snoowrap
 
 def number_of_subscribers(subreddit):
-    
-"""Return number of subscribers in the subreddit."""
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    If an invalid subreddit is given, the function returns 0.
 
-    response = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit))
-    if response.statuscode == 200:
-        return response["subscribers"]
-    else:
-        return 0
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers for the given subreddit, or 0 if the subreddit is invalid.
+    """
+    # Set up the Reddit API wrapper with the provided credentials
+    r = snoowrap({
+        'userAgent': 'My custom user agent',
+        'clientId': '<client_id>',
+        'clientSecret': '<client_secret>'
+    })
+
+    # Get the user's subscriptions
+    subscriptions = r.getSubscriptions()
+
+    # Check if the provided subreddit is in the user's subscriptions
+    for subscription in subscriptions:
+        if subscription.display_name == subreddit:
+            return subscription.subscribers
+
+    # If the subreddit is not found, return 0
+    return 0
