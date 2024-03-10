dc5d7f652de178f392bd39719b3e6c3456c5f032
Palvis23 <139566126+Pntawukamenya@users.noreply.github.com>
2024-03-10 16:56:16 +0200

    Commit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -8,27 +8,33 @@ the function should return 0.
 """
 import requests
 
+def get_emp_todo(emp_id):
+    TODO_URL = f"https://jsonplaceholder.typicode.com/users/{emp_id}/todos"
+    USER_URL = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
+    
+    # Get the JSON of TODOS - DONE
+    # Get TOTAL NUMBER of todos - DONE
+    # Get COMPLETED NUMBER of todos - DONE
+    # Get THE NAME of the employee - DONE 
 
-def number_of_subscribers(subreddit):
-    """Returns the total number of subscribers
-    for a given subreddit.
-    """
-    # Set the Default URL strings
-    headers = {'User-Agent': 'my-app/0.0.1'}
-    base_url = 'https://www.reddit.com'
-    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
-                                                       subreddit=subreddit)
+    # GET THE TITLES OF THE COMPLETED TASKS
+    # AND PRINT THEM ALL OUT ON STDOUT
+    # EACH TASKS SHOULD BE PRINTED ON A NEW LINE
 
-    # Set an User-Agent
-    user_agent = {'User-Agent': 'Python/requests'}
 
-    # Get the Response of the Reddit API
-    res = requests.get(api_uri, headers=headers,
-                       allow_redirects=False)
+    emp_todos = requests.get(TODO_URL).json()
+    emp_name = requests.get(USER_URL).json()["name"]
+    emp_total_todos = len(emp_todos)
+    emp_completed_todos = 0
+    todo_titles = []
 
-    # Checks if the subreddit is invalid
-    if res.status_code in repr([302, 404]):
-        return 0
+    for todo in emp_todos:
+        if todo["completed"] is True:
+            todo_titles.append(todo["title"])
+            emp_completed_todos += 1
+    print(f"Employee {emp_name} From total todos of {emp_total_todos}, {emp_completed_todos} tasks have been done")
 
-    # Returns the total subscribers of the subreddit
-    return res.json().get('data').get('subscribers')
+    for todo_title in todo_titles:
+        print(todo_title)
+
+get_emp_todo(1)
