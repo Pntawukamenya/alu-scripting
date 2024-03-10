d4bf43db19104fbddfe24ee3c70ff00532722215
Palvis23 <139566126+Pntawukamenya@users.noreply.github.com>
2024-03-10 17:44:21 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python3
-"""Return number of subscribers for a given subreddit"""
+# -*- coding: utf-8 -*-
+
+""" Returns the number of subscribers for a given subreddit. """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers """
-    url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
+    """ Returns the number of subscribers for a given subreddit. """
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = \
+        {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)\
+          AppleWebKit/537.36(KHTML, like Gecko) \
+         Chrome/90.0.4430.93 Safari/537.36'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        return response.json().get('data') \
-            .get('subscribers')
-    return 0
+        return response.json().get('data').get('subscribers')
+    else:
+        return 0
+
+
+if __name__ == '__main__':
+    print(number_of_subscribers)('programming')
+    print(number_of_subscribers)('not_a_valid_subreddit')
